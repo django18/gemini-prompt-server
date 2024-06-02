b08316056a7a50e0e53d456b575c9ce035d3e9d6
Avinash Dangi <avinashdangi@Avinashs-MacBook-Pro.local>
2024-06-02 12:19:58 +0530

    gemini api server

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+import { run } from "./model";
+
+app.get("/", (req, res) => {
+  console.log({ req });
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
---
+++ model.js
@@ -0,0 +1,19 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+export async function run(prompt) {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  // const prompt = "Give me a 5 day itinerary for south goa.";
+  try {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  } catch (error) {
+    console.log({ error });
+  }
+}
